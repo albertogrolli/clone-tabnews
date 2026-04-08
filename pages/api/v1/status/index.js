cbfbfe3c6e010add7c301519e5ff5f32ec6b1a10
albertogrolli <albertogrolli@hotmail.com>
2026-04-08 12:00:32 +0000

    makes `database.js` `client.connect()` more robust to errors

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,7 +4,6 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
-  // console.log("console", databaseVersionResult.rows[0]);
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
   const databaseMaxConnectionsResult = await database.query(
