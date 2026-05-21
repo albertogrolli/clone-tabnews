f287af22cf093b5e0f13abf4fa403dce63b3d0c1
albertogrolli <albertogrolli@hotmail.com>
2026-05-21 23:14:00 +0000

    feat: add default feature `read:activation_token` when creating `user`

--- models/user.js
+++ models/user.js
@@ -183,6 +183,7 @@ async function create(userInputValues) {
   await validateUniqueUsername(userInputValues.username);
   await validateUniqueEmail(userInputValues.email);
   await hashPasswordInObject(userInputValues);
+  injectDefaultFeaturesInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
@@ -191,9 +192,9 @@ async function create(userInputValues) {
     const results = await database.query({
       text: `
       INSERT INTO
-      users (username, email, password)
+      users (username, email, password, features)
       VALUES
-      ($1, $2, $3)
+      ($1, $2, $3, $4)
       RETURNING
       *
       ;`,
@@ -201,10 +202,15 @@ async function create(userInputValues) {
         userInputValues.username,
         userInputValues.email,
         userInputValues.password,
+        userInputValues.features,
       ],
     });
 
     return results.rows[0];
+  }
+
+  function injectDefaultFeaturesInObject(userInputValues) {
+    userInputValues.features = ["read:activation_token"];
   }
 }
 
--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -29,7 +29,7 @@ describe("Use case: Registration Flow (all successful)", () => {
       id: createUserResponseBody.id,
       username: "RegistrationFlow",
       email: "registration@flow.com",
-      features: [],
+      features: ["read:activation_token"],
       password: createUserResponseBody.password,
       created_at: createUserResponseBody.created_at,
       updated_at: createUserResponseBody.updated_at,
