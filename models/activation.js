2c40c676697b28f4005a17bd71ad57d8136795a0
albertogrolli <albertogrolli@hotmail.com>
2026-05-22 23:38:04 +0000

    feat: add `activation.findOneValidById()` and `orchestrator.extractUUID()`

---
+++ models/activation.js
@@ -0,0 +1,80 @@
+import database from "infra/database";
+import email from "infra/email.js";
+import { NotFoundError } from "infra/errors";
+import webServer from "infra/webServer.js";
+
+const EXPIRATION_IN_MILLISECONDS = 50 * 15 * 1000;
+
+async function findOneValidById(tokenId) {
+  const activationTokenObject = await runSelectQuery(tokenId);
+  return activationTokenObject;
+
+  async function runSelectQuery(tokenId) {
+    const results = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          user_activation_tokens
+        WHERE
+          id = $1
+          AND expires_at > NOW()
+          AND used_at IS NULL
+        LIMIT
+          1
+        ;`,
+      values: [tokenId],
+    });
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message:
+          "O token de ativação utilizado não foi encontrado no sistema ou expirou.",
+        action: "Faça um novo cadastro.",
+      });
+    }
+    return results.rows[0];
+  }
+}
+
+async function create(userId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+  const newToken = await runInsertQuery(userId, expiresAt);
+  return newToken;
+
+  async function runInsertQuery(userId, expiresAt) {
+    const results = await database.query({
+      text: `
+        INSERT INTO
+          user_activation_tokens (user_id, expires_at)
+        VALUES
+          ($1, $2)
+        RETURNING
+          *
+    ;`,
+      values: [userId, expiresAt],
+    });
+    return results.rows[0];
+  }
+}
+
+async function sendEmailToUser(user, activationToken) {
+  await email.send({
+    from: '"Alberto Grolli" <contato@albertogrolli.com.br>',
+    to: user.email,
+    subject: "Ative seu cadastro",
+    text: `${user.username}, clique no link abaixo para ativar o seu cadastro:
+
+${webServer.origin}/cadastro/ativar/${activationToken.id}/
+
+Atenciosamente,
+Alberto Grolli`,
+  });
+}
+
+const activation = {
+  create,
+  sendEmailToUser,
+  findOneValidById,
+};
+
+export default activation;
