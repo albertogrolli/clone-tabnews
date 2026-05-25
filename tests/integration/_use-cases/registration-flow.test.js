28781bb46133c8fd637369bcc0661a021e2426eb
albertogrolli <albertogrolli@hotmail.com>
2026-05-25 14:42:52 +0000

    feat: add `injectAnonymousOrUser` and `canRequest` middlewares to `/sessions`

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -80,7 +80,21 @@ describe("Use case: Registration Flow (all successful)", () => {
     expect(activatedUser.features).toEqual(["create:session"]);
   });
   test("Login", async () => {
-    //
+    const createSessionResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: "registration@flow.com",
+          password: "password",
+        }),
+      },
+    );
+    expect(createSessionResponse.status).toBe(201);
+    const createSessionResponseBody = await createSessionResponse.json();
+
+    console.log(createSessionResponseBody);
   });
   test("Get user information", async () => {
     //
